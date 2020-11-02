3ae6938111f1a8e5a8ba568063aa420d854b56aa
joscript <josua.lagat@gmail.com>
2020-11-02 20:34:09 +0800

    object destructuring

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,22 +4,26 @@ import Counter from './counter';
 class Counters extends Component {
 
     render() { 
+
+        // object destructuring
+        const {onReset, onDelete, onIncrement, onDecrement} = this.props;
+
         return ( 
             <div className="mt-2 container">
                 <button 
                     className="btn btn-info btn-sm m-2"
-                    onClick={this.props.onReset}
+                    onClick={onReset}
                 >
                     Reset
                 </button>
                 { 
                     this.props.counters.map( 
                         counter => <Counter 
-                                        onDelete={this.props.onDelete} 
+                                        onDelete={onDelete} 
                                         key={counter.id} 
                                         counter={counter}
-                                        onIncrement={this.props.onIncrement}
-                                        onDecrement={this.props.onDecrement}
+                                        onIncrement={onIncrement}
+                                        onDecrement={onDecrement}
                                     /> 
                     )   
                 }
