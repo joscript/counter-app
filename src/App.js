b18459fd9d0bd92f84cc72a71970d8bca74614c0
joscript <josua.lagat@gmail.com>
2020-11-02 20:13:45 +0800

    lifted states up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
 import Navbar from './components/navbar';
 import Counter from './components/counters';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <main className="container"> 
-        <Counter />
-      </main>
-    </div>
-  );
+class App extends Component {
+
+    state = { 
+        counters: [
+            {id: 1, value: 1},
+            {id: 2, value: 0},
+            {id: 3, value: 4},
+            {id: 4, value: 0}
+        ]
+    };
+
+    handleDelete = (counterId) => {
+        // console.log('Event handle called', counterId);
+        const counters = this.state.counters.filter(counter => counter.id !== counterId);
+        this.setState({counters});
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(counter => {
+            counter.value = 0;
+            return counter;
+        })
+
+        this.setState({counters})
+    }
+
+    handleIncrement = (data) => {
+        // console.log(data)
+        const counters = [...this.state.counters]; // used spread operator to clone 
+        const index = counters.indexOf(data);
+        counters[index] = {...data}; // to refer only to the object given and not to the local state (kase naa-update nya pala)
+        counters[index].value++;
+        // console.log(this.state.counters[index].value)
+
+        this.setState({counters});
+    }
+
+    handleDecrement = (data) => {
+        const counters = [...this.state.counters]; // used spread operator to clone 
+        const index = counters.indexOf(data);
+        counters[index] = {...data}; // to refer only to the object given and not to the local state (kase naa-update nya pala)
+        counters[index].value--;
+
+        this.setState({counters});
+    }
+  
+    render() {
+      return (
+        <div className="App">
+          <Navbar />
+          <main className="container"> 
+            <Counter 
+              onReset={this.handleReset}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
+              counters={this.state.counters}
+            />
+          </main>
+        </div>
+      );
+    }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,71 +2,28 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            {id: 1, value: 1},
-            {id: 2, value: 0},
-            {id: 3, value: 4},
-            {id: 4, value: 0}
-        ]
-    };
-
-    handleDelete = (counterId) => {
-        // console.log('Event handle called', counterId);
-        const counters = this.state.counters.filter(counter => counter.id !== counterId);
-        this.setState({counters});
-    }
-
-    handleReset = () => {
-        const counters = this.state.counters.map(counter => {
-            counter.value = 0;
-            return counter;
-        })
-
-        this.setState({counters})
-    }
-
-    handleIncrement = (data) => {
-        // console.log(data)
-        const counters = [...this.state.counters]; // used spread operator to clone 
-        const index = counters.indexOf(data);
-        counters[index] = {...data}; // to refer only to the object given and not to the local state (kase naa-update nya pala)
-        counters[index].value++;
-        // console.log(this.state.counters[index].value)
-
-        this.setState({counters});
-    }
-
-    handleDecrement = (data) => {
-        const counters = [...this.state.counters]; // used spread operator to clone 
-        const index = counters.indexOf(data);
-        counters[index] = {...data}; // to refer only to the object given and not to the local state (kase naa-update nya pala)
-        counters[index].value--;
-
-        this.setState({counters});
-    }
 
     render() { 
         return ( 
             <div className="mt-2 container">
                 <button 
                     className="btn btn-info btn-sm m-2"
-                    onClick={this.handleReset}
+                    onClick={this.props.onReset}
                 >
                     Reset
                 </button>
                 { 
-                    this.state.counters.map( 
+                    this.props.counters.map( 
                         counter => <Counter 
-                                        onDelete={this.handleDelete} 
+                                        onDelete={this.props.onDelete} 
                                         key={counter.id} 
                                         counter={counter}
-                                        onIncrement={this.handleIncrement}
-                                        onDecrement={this.handleDecrement}
+                                        onIncrement={this.props.onIncrement}
+                                        onDecrement={this.props.onDecrement}
                                     /> 
                     )   
                 }
-            </div>
+            </div>  
          );
     }
 }
